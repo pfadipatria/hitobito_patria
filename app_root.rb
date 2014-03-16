49dad1ac6aa16f3d1bf6e5976d38c7aaecc70923
NicolasKessler <janovethejackal@gmail.com>
2014-03-16 11:11:28 +0100

    Cleaned

--- app_root.rb
+++ app_root.rb
@@ -1,4 +1,3 @@
 # define the root directory of the main application
-#ENV['APP_ROOT'] ||= File.expand_path(__FILE__).split("vendor#{File::SEPARATOR}wagons").first
 ENV['APP_ROOT'] ||= File.expand_path('../../hitobito', __FILE__)
-#ENV['APP_ROOT'] ||= File.expand_path(__FILE__).split("app_root.rb").first
+
