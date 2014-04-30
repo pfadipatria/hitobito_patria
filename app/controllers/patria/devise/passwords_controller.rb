192676b9a9dfda9cfef587b0a7f64e4466d3f42e
theodortruffer <theodortruffer@bluewin.ch>
2014-04-30 16:40:30 +0200

    Added Mailer to recover PW

--- app/controllers/patria/devise/passwords_controller.rb
+++ app/controllers/patria/devise/passwords_controller.rb
@@ -21,8 +21,15 @@ module Patria::Devise::PasswordsController
           end
           
         else 
-          flash[:alert] = config['no_ldap_user']
-          redirect_to(:back)
+          self.resource = resource_class.send_reset_password_instructions(resource_params)
+          yield resource if block_given?
+  
+          if successfully_sent?(resource)
+            respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+          else
+            respond_with(resource)
+          end
+
         end
       end
     end
