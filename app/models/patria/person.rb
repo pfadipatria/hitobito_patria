28dafc535ebdf92cfbcef492b4c01f9c46f8ac56
Icewater1337 <fuchsmat@students.unibe.ch>
2015-03-30 13:30:07 +0200

    changed so that it searches parameters by email on ldap

--- app/models/patria/person.rb
+++ app/models/patria/person.rb
@@ -8,19 +8,19 @@ module Patria::Person
   end
   
   def get_ldap_first_name
-    firstname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"givenName")
+    firstname = Devise::LDAP::Adapter.get_ldap_param(self.email,"givenName")
     self.first_name = firstname.first unless firstname == nil;
   rescue Net::LDAP::LdapError
   end
    
   def get_ldap_last_name
-    lastname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"sn")
+    lastname = Devise::LDAP::Adapter.get_ldap_param(self.email,"sn")
     self.last_name = lastname.first unless lastname == nil;
   rescue Net::LDAP::LdapError
   end
 
   def ldap_user?
-    Devise::LDAP::Adapter.get_ldap_param(self.nickname,"uid") != nil
+    Devise::LDAP::Adapter.get_ldap_param(self.email,"uid") != nil
   end
 
 end
