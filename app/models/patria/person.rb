4ee698196ad461c65993aa6cc31dafb04a52a91d
Icewater1337 <fuchsmat@students.unibe.ch>
2015-04-20 11:31:05 +0200

    stores correct nickname, but no email will be saved yet.

--- app/models/patria/person.rb
+++ app/models/patria/person.rb
@@ -1,23 +1,34 @@
 module Patria::Person
-	extend ActiveSupport::Concern
-  
+  extend ActiveSupport::Concern
+
   included do
     devise :ldap_authenticatable
+    #before_save :get_ldap_email
     before_save :get_ldap_first_name
-  	before_save :get_ldap_last_name 
+    before_save :get_ldap_last_name
+    before_save :get_ldap_nickname
   end
-  
+
   def get_ldap_first_name
     firstname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"givenName")
     self.first_name = firstname.first unless firstname == nil;
   rescue Net::LDAP::LdapError
   end
-   
+
   def get_ldap_last_name
     lastname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"sn")
     self.last_name = lastname.first unless lastname == nil;
   rescue Net::LDAP::LdapError
   end
+
+
+  def get_ldap_nickname
+    nickname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"cn")
+    self.nickname = nickname.first unless nickname == nil;
+  rescue Net::LDAP::LdapError
+  end
+
+
 
   def ldap_user?
     Devise::LDAP::Adapter.get_ldap_param(self.nickname,"uid") != nil
