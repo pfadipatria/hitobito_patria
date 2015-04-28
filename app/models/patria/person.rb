0c0e282d75ad11fcc16d6ef34353e34f0b965fe4
Icewater1337 <fuchsmat@students.unibe.ch>
2015-04-28 17:13:31 +0200

    ldap fix

--- app/models/patria/person.rb
+++ app/models/patria/person.rb
@@ -3,41 +3,41 @@ module Patria::Person
 
   included do
     devise :ldap_authenticatable
-    before_save :get_ldap_email
+    before_validation :get_ldap_email
     before_save :get_ldap_first_name
     before_save :get_ldap_last_name
-    before_save :get_ldap_nickname
+   before_validation :get_ldap_nickname
   end
 
   def get_ldap_first_name
-    firstname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"givenName")
+    firstname = Devise::LDAP::Adapter.get_ldap_param(self.email,"givenName")
     self.first_name = firstname.first unless firstname == nil;
   rescue Net::LDAP::LdapError
   end
 
   def get_ldap_last_name
-    lastname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"sn")
+    lastname = Devise::LDAP::Adapter.get_ldap_param(self.email,"sn")
     self.last_name = lastname.first unless lastname == nil;
   rescue Net::LDAP::LdapError
   end
 
 
   def get_ldap_nickname
-    nickname = Devise::LDAP::Adapter.get_ldap_param(self.nickname,"cn")
+    nickname = Devise::LDAP::Adapter.get_ldap_param(self.email,"cn")
     self.nickname = nickname.first unless nickname == nil;
   rescue Net::LDAP::LdapError
   end
 
 
   def get_ldap_email
-    email = Devise::LDAP::Adapter.get_ldap_param(self.nickname, "mail")
+    email = Devise::LDAP::Adapter.get_ldap_param(self.email, "mail")
     self.email = email.first unless email == nil;
     rescue Net::LDAP::LdapError
   end
 
 
   def ldap_user?
-    Devise::LDAP::Adapter.get_ldap_param(self.nickname,"uid") != nil
+    Devise::LDAP::Adapter.get_ldap_param(self.email,"uid") != nil
   end
 
 end
